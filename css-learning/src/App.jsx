7c6378302d5dfae3064c6cd051b435ae3d00c319
AronM <aronddtt@gmail.com>
2024-06-26 10:04:36 +0200

    eleat-copy-v1

---
+++ css-learning/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavBar from "./Nav-bar";
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  );
+}
+
+export default App;
---
+++ css-learning/src/Nav-bar.jsx
@@ -0,0 +1,41 @@
+export default function NavBar() {
+  return (
+    <>
+      <div className="container">
+        <div className="notif-bar-div">
+          <h1 className="notif-bar">FREE POUCH ON ORDERS OVER &#163;40</h1>
+        </div>
+        <nav className="nav-bar">
+          <div className="left-nav">
+            <a href="#">
+              <button className="shop">SHOP</button>
+            </a>
+            <a className="build-a-bundle" href="#">
+              BUILD A BUNDLE
+            </a>
+          </div>
+          <div className="logo">
+            <img src="/ELEAT-logo.svg" alt="site-logo" />
+          </div>
+          <div className="right-nav">
+            <ul className="right-nav-content">
+              <li className="our-story">
+                <a href="#">OUR STORY</a>
+              </li>
+              <li className="refer-a-friend">
+                <a href="#">REFER A FRIEND</a>
+              </li>
+              <li className="user-icon">
+                <img src="/User.svg" alt="user-icon" />
+              </li>
+              <li className="cart-icon">
+                <img src="/Cart.svg" alt="cart-icon" />
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <section className="hero"></section>
+      </div>
+    </>
+  );
+}
