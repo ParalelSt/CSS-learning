70293e9490ab10276ba6eb9e40fa7df083a342ab
AronM <strixparalel@gmail.com>
2024-06-27 18:50:06 +0200

    eleat-copy-v5

--- css-learning/src/Hero.jsx
+++ css-learning/src/Hero.jsx
@@ -3,11 +3,20 @@ export default function Hero() {
     <div className="hero-container">
       <section className="hero">
         <div className="left-section">
-          <div className="reviews-container"></div>
-          <h1 className="section-title">
-            New Caramelised Biscuit flavour live!
-          </h1>
-          <h3 className="section-comment">Fuel your day with protein cereal</h3>
+          <div className="reviews-container">
+            <span className="star-count">
+              <img src="Star.svg" alt="" />
+              <img src="Star.svg" alt="" />
+              <img src="Star.svg" alt="" />
+              <img src="Star.svg" alt="" />
+              <img src="Star.svg" alt="" />
+            </span>
+            <span className="review-section">
+              4.7 FROM 3300+ HAPPY CUSTOMERS
+            </span>
+          </div>
+          <p className="section-title">New Caramelised Biscuit flavour live!</p>
+          <p className="section-comment">Fuel your day with protein cereal</p>
           <div className="section-images">
             <img src="icon-high-protein.webp" alt="high-protein" />
             <img src="icon-high-fibre.webp" alt="high-fibre" />
@@ -25,6 +34,8 @@ export default function Hero() {
               alt="vegan-friendly"
             />
           </div>
+          <button className="shop-now-btn">SHOP NOW</button>
+          <p>Join 40,000+ who made the switch</p>
         </div>
         <div className="right-section"></div>
         <div className="right-section">
