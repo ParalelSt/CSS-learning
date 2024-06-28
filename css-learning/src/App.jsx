a75a52d1763edb6990f1d01dc1d6ce998e17f34f
AronM <strixparalel@gmail.com>
2024-06-28 10:33:19 +0200

    eleat-copy-v7

--- css-learning/src/App.jsx
+++ css-learning/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./Nav-bar";
 import Hero from "./Hero";
+import Footer from "./Footer";
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
       <Hero></Hero>
+      <Footer></Footer>
     </>
   );
 }
---
+++ css-learning/src/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer-text">AS SEEN IN</p>
+      <div className="ad-cycle"></div>
+      <div className="white-bar"></div>
+    </div>
+  );
+}
