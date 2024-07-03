728e91036c59f768cb066dc7927ea352e5e5ca58
AronM <strixparalel@gmail.com>
2024-07-03 08:28:11 +0200

    eleat-copy-v8

--- css-learning/src/Nav-bar.jsx
+++ css-learning/src/Nav-bar.jsx
@@ -28,10 +28,97 @@ export default function NavBar() {
               <li className="user-icon">
                 <img src="/User.svg" alt="user-icon" />
               </li>
+              <button className="hamburger">
+                <img className="hamburger-menu" src="DropDWN.svg" alt="" />
+              </button>
               <li className="cart-icon">
                 <img src="/Cart.svg" alt="cart-icon" />
               </li>
             </ul>
+          </div>
+
+          <div className="mobile-nav">
+            <div className="items-display">
+              <div className="item item1">
+                <span>
+                  <div className="img-container">
+                    <img src="item1.webp" alt="" />
+                  </div>
+                  <div className="item-container">
+                    <p className="item-notification">POPULAR</p>
+                    <p className="item-title">CEREAL POUCHES</p>
+                    <p className="item-description">
+                      250mg multi-serve pouches
+                    </p>
+                  </div>
+                </span>
+              </div>
+              <div className="item item2">
+                <span>
+                  <div className="img-container">
+                    <img src="item2.webp" alt="" />
+                  </div>
+                  <div className="item-container">
+                    <p className="item-notification">ON THE GO</p>
+                    <p className="item-title">CEREAL POTS</p>
+                    <p className="item-description">
+                      50g pots for when you're on the go
+                    </p>
+                  </div>
+                </span>
+              </div>
+              <div className="item item3">
+                <span>
+                  <div className="img-container">
+                    <img src="item3.webp" alt="" />
+                  </div>
+                  <div className="item-container">
+                    <p className="item-notification">BEST SELLER</p>
+                    <p className="item-title">BUILD A BUNDLE</p>
+                    <p className="item-description">
+                      Mix & match your favorite flavors
+                    </p>
+                  </div>
+                </span>
+              </div>
+              <div className="item item4">
+                <span>
+                  <div className="img-container">
+                    <img src="item4.webp" alt="" />
+                  </div>
+                  <div className="item-container">
+                    <p className="item-notification">NEW</p>
+                    <p className="item-title">NEW FLAVOUR: MOCHA</p>
+                    <p className="item-description">
+                      One for the coffee lovers out there!
+                    </p>
+                  </div>
+                </span>
+              </div>
+            </div>
+            <span>
+              <a href="#">Give 10&#163;, Get 10&#163; </a>
+              <img className="item-arrow" src="Arrow.svg" alt="" />
+            </span>
+            <span>
+              <a href="#">Our Story</a>
+              <img className="item-arrow" src="Arrow.svg" alt="" />
+            </span>
+            <span>
+              <a href="#">Reviews</a>
+              <img className="item-arrow" src="Arrow.svg" alt="" />
+            </span>
+            <span>
+              <a href="#">My Account</a>
+              <img className="item-arrow" src="Arrow.svg" alt="" />
+            </span>
+            <span>
+              <a href="#">Student Discount</a>
+              <img className="item-arrow" src="Arrow.svg" alt="" />
+            </span>
+            <div className="display">
+              <p>START WITH A VARIETY PACK</p>
+            </div>
           </div>
         </nav>
       </div>
